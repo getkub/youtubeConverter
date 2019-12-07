ebaf03ea40abaa42ea29ccd18a44ee38d72753fd
getkub <kinbsk@gmail.com>
2019-12-07 19:15:58 +0000

    Updated rename Script

--- others/renameFile.sh
+++ others/renameFile.sh
@@ -1,18 +1,32 @@
 dirLocation="../../convertedFiles"
-fileFilter="mp3"
+fileFilters="mp3 mp4 m4a"
 
 # Changes spaces, brackets => underscore
 # strips off quotes, comma
+
+
+#\x27 - single
+#\x22 - double
+
+rename_file_extension()
+{
 OIFS="$IFS"
 IFS=$'\n'
-
-for i in `ls ${dirLocation}/*.${fileFilter}`
+for i in `ls ${dirLocation}/*.${1} 2>/dev/null`
 do
     baseDir=$(dirname $i)
     filename=$(basename $i)
     #echo filename=$filename
     newName=`echo $filename | awk '{gsub(/(\s+|\)|\()/,"_");}1' `
+    newName=`echo $newName | awk '{gsub(/(\x27|\x22)/,"");}1'`
     echo "Renaming $i .."
     mv "${baseDir}/${filename}" ${baseDir}/${newName}
 done
 IFS="$OIFS"
+}
+
+# Pass each fileFilters to rename function
+for fileFilter in $fileFilters
+do
+    rename_file_extension $fileFilter
+done
