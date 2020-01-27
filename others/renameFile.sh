f3a11ee8fc90b8d5eb2cadcfbffa79bf539e9c35
KK <kin@kmb.local>
2020-01-27 10:14:47 +0000

    draft version of rename script

--- others/renameFile.sh
+++ others/renameFile.sh
@@ -1,5 +1,8 @@
+# This script is under-development and needs fixing
+
 dirLocation="../../convertedFiles"
 fileFilters="mp3 mp4 m4a"
+patternsList="regexPatterns.list"
 
 # Changes spaces, brackets => underscore
 # strips off quotes, comma
@@ -16,11 +19,15 @@ for i in `ls ${dirLocation}/*.${1} 2>/dev/null`
 do
     baseDir=$(dirname $i)
     filename=$(basename $i)
-    #echo filename=$filename
-    newName=`echo $filename | awk '{gsub(/(\s+|\)|\()/,"_");}1' `
-    newName=`echo $newName | awk '{gsub(/(\x27|\x22)/,"");}1'`
-    echo "Renaming $i .."
-    mv "${baseDir}/${filename}" ${baseDir}/${newName}
+    newName=$filename
+    #echo "Filename is: $newName"
+    for pattern in `cat ${patternsList}|egrep -v '^#'|egrep '^[[:space:]]*$'`
+    do
+      echo "Applying Pattern $pattern => to $newName"
+      newName=`echo $newName | awk "{gsub${pattern};}1" `
+    done
+    echo "Renaming $i ==> $newName"
+    # mv "${baseDir}/${filename}" ${baseDir}/${newName}
 done
 IFS="$OIFS"
 }
